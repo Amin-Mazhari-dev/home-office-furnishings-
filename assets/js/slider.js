81d455e164618103c4be8a13ef170dad79d9d7d1
Amin Mazhari <amin.mazhari.1981@gmail.com>
2025-11-13 11:50:20 +0330

    Add JS codes for slider

---
+++ assets/js/slider.js
@@ -0,0 +1,114 @@
+const nextImg = document.querySelector('.next')
+const prevImg = document.querySelector('.prev')
+const containerSlider = document.querySelector('.container-slider')
+const imgSlid = document.querySelector('.img-slid')
+const sliderItem = document.querySelector('.slider-item')
+
+let imgArray = ['assets/img/Slide--Image--1.jpg','assets/img/Slide--Image--2.jpg','assets/img/Slide--Image--3.jpg','assets/img/Slide--Image--4.jpg','assets/img/Slide--Image--5.jpg']
+let imgIndex = 0
+
+let btnFragment = document.createDocumentFragment()
+let allButtons = []
+
+let seeMoreBtnFragment = document.createDocumentFragment()
+let allSeeMoreBtn = []
+
+imgArray.forEach(function(items, index){
+
+    const newBtn = document.createElement('button')
+    newBtn.setAttribute('type','button')
+    newBtn.classList.add('btn-slider')
+    
+    newBtn.dataset.index = index
+
+    allButtons.push(newBtn)
+    btnFragment.append(newBtn)
+
+    newBtn.addEventListener('click', function(event){
+
+        allButtons.forEach(function(btnItems){
+
+            btnItems.classList.remove('active')
+        })
+
+        newBtn.classList.add('active')
+
+        let imageIndex = parseInt(event.target.dataset.index)
+        imgSlid.setAttribute('src', imgArray[imageIndex])
+    })
+    
+})
+containerSlider.append(btnFragment)
+
+allButtons[0].classList.add('active')
+imgSlid.setAttribute('src', imgArray[0])
+
+
+nextImg.addEventListener('click', function(){
+
+    imgIndex ++
+
+    if(imgIndex > imgArray.length - 1){
+
+        imgIndex = 0
+    }
+    imgSlid.setAttribute('src', imgArray[imgIndex])
+    updateActiveButton()
+
+})
+
+prevImg.addEventListener('click', function(){
+
+    imgIndex --
+
+    if(imgIndex < 0){
+
+        imgIndex = imgArray.length - 1
+    }
+    imgSlid.setAttribute('src', imgArray[imgIndex])
+    updateActiveButton()
+
+})
+
+//........... This Functions update Active Button
+
+function updateActiveButton(){
+
+    allButtons.forEach(function(btn){
+
+        btn.classList.remove('active')
+
+        if(parseInt(btn.dataset.index) === imgIndex){
+
+            btn.classList.add('active')
+        }
+    })
+}
+
+// ....... creates 'seen more button'
+
+imgArray.forEach(function(items){
+
+    const seeMoreBtn = document.createElement('a')
+    seeMoreBtn.setAttribute('href', '#')
+    seeMoreBtn.setAttribute('id', 'more-see')
+    seeMoreBtn.classList.add('absolute')
+    seeMoreBtn.innerHTML = 'مشــــاهـــده بیشتـــر ...'
+
+    allSeeMoreBtn.push(seeMoreBtn)
+    seeMoreBtnFragment.append(seeMoreBtn)
+})
+sliderItem.append(seeMoreBtnFragment)
+
+setInterval( function(){
+    
+    imgIndex ++
+
+    if(imgIndex > imgArray.length - 1){
+
+        imgIndex = 0
+    }
+    imgSlid.setAttribute('src', imgArray[imgIndex])
+    updateActiveButton()
+
+}, 7000);
