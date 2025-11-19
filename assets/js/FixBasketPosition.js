28fbbe95fab688e9a006dac33745abf1c51f8036
Amin Mazhari <amin.mazhari.1981@gmail.com>
2025-11-19 10:32:23 +0330

    Added (FixBasketPosition.js)

---
+++ assets/js/FixBasketPosition.js
@@ -0,0 +1,19 @@
+const newbasketCardSection = document.querySelector('.basket-card-section')
+
+function getElementTop(element) {
+    const style = getComputedStyle(element);
+    const topValue = style.top; 
+    
+    if (!topValue || topValue === "auto") return 0;
+    
+    return parseFloat(topValue.replace(/px|rem|em|%/g, '')) || 0;
+}
+let currentTop = getElementTop(newbasketCardSection);
+
+window.addEventListener('scroll', function(event){
+
+    let myscroll = document.documentElement.scrollTop
+
+    let newTop = currentTop + myscroll
+    newbasketCardSection.style.top = newTop + "px";
+})
