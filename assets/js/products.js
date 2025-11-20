3fe99300b995031132f54c26165295beeddf7134
Amin Mazhari <amin.mazhari.1981@gmail.com>
2025-11-20 17:21:55 +0330

    Moved (productsArray) to products-data.js for ES6 Modules

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,3 +1,5 @@
+import {productsArray} from './products-data.js';
+
 const cardOneImg = document.querySelector('.card-one-img')
 const cardTwoImg = document.querySelector('.card-two-img')
 const cardThreeImg = document.querySelector('.card-three-img')
@@ -8,89 +10,6 @@ const showProductPrice = document.getElementById('show-product-price')
 const aboutProductName = document.getElementById('about-product-name')
 const aboutProductDescription = document.getElementById('about-product-description')
 
-
-let productsArray = [
-
-    {
-        codeProduct: 1,
-        section: 'slider',
-        productName: 'Siena Calm',
-        productStatus: 'موجود',
-        productPrice: 1550000,
-        productImg: [
-
-            'assets/img/Siena Calm - 01.jpg',
-            'assets/img/Siena Calm - 02.png',
-            'assets/img/Siena Calm - 03.png'
-        ],
-        productDescription: 'مبل پنج ‌نفره‌ی Siena Calm، تجسمی از آرامش و سادگی‌ست؛ جایی برای نشستن، نفس کشیدن، و بازگشت به خویشتن. رنگ گرم و خاکی آن، یادآور غروب‌های سیه‌ناست؛ جایی که نور و سکوت در آغوش هم می‌نشینند. طراحی مینیمال با خطوط نرم و پایه‌های فلزی مشکی، ترکیبی از مدرنیته و لطافت را به فضای شما هدیه می‌دهد. پارچه‌ی مخملی و بافت‌دار آن، نه تنها چشم‌نواز است، بلکه حس لمس طبیعت را در دل خانه جاری می‌سازد. همراهی با میز سبز و گلدان ارکیده، این محصول را به نقطه‌ی کانونی هر اتاق تبدیل می‌کند. Siena Calm نه فقط یک مبل، بلکه یک دعوت است—به سکون، به زیبایی، به لحظه‌ای برای خود بودن. مناسب برای فضاهای مدرن، مینیمال، یا حتی کلاسیک با چاشنی آرامش و تعادل. با Siena Calm، هر نشستن، یک تجربه‌ی شاعرانه خواهد بود.',
-        urlParams: "id=1&title=Siena Calm"
-    },
-
-    {
-        codeProduct: 2,
-        section: 'slider',
-        productName: 'California Puff',
-        productStatus: 'موجود',
-        productPrice: 1700000,
-        productImg: [
-
-            'assets/img/California Puff - 01.jpg',
-            'assets/img/California Puff - 02.png',
-            'assets/img/California Puff - 03.png'
-        ],
-        productDescription: 'مبل و صندلی‌های California Puff، تجسمی از راحتی بی‌مرز و طراحی مدرن هستند. با رنگ کاراملی گرم و بافت چرمی لطیف، این محصول حس آغوشی نرم را به فضای شما می‌بخشد.طراحی پف‌دار و خطوط منحنی، آرامش را در هر نشستن جاری می‌کند.پایه‌های فلزی مشکی، تضادی هنرمندانه با فرم نرم و حجیم صندلی‌ها ایجاد کرده‌اند.مناسب برای فضاهای مدرن، مینیمال، یا حتی صنعتی با چاشنی گرما و صمیمیت. California Puff نه فقط یک مبلمان، بلکه یک تجربه‌ی لمسی و بصری‌ست.همراهی با نورپردازی ملایم و عناصر دکوراتیو، این محصول را به نقطه‌ی کانونی هر اتاق تبدیل می‌کند. با California Puff، هر لحظه‌ی نشستن، سفری‌ست به آرامش و زیبایی.',
-        urlParams: "id=2&title=California Puff"
-    },
-
-    {
-        codeProduct: 3,
-        section: 'slider',
-        productName: 'Berlin Flex',
-        productStatus: 'موجود',
-        productPrice: 7000000,
-        productImg: [
-
-            'assets/img/Berlin Flex - 01.jpg',
-            'assets/img/Berlin Flex - 02.png',
-            'assets/img/Berlin Flex - 03.png'
-        ],
-        productDescription: 'سری مبلمان مدولار Berlin Flex، تجسمی از طراحی مینیمال و عملکرد هوشمندانه است. با رنگ مشکی کلاسیک و فرم‌های ساده، این محصول هر فضایی را به نهایت سادگی و زیبایی می‌رساند. قابلیت تبدیل شدن به لانژ، به شما آزادی می‌دهد که فضای خود را با لمس یک حرکت تغییر دهید. پایه‌های فلزی مشکی، ساختاری استوار و مدرن به طراحی این مبلمان بخشیده‌اند. محصولی مناسب برای خانه‌ها، ادارات، یا استودیوهایی که در جستجوی تعادل بین فرم و عملکرد هستند. Berlin Flex نه فقط یک مبلمان، بلکه یک هارمونی از نهایت سادگی و نهایت انعطاف است. مناسب برای فضاهای صنعتی، مدرن، یا حتی کوچک که در پی استفاده‌ی بهینه از فضا هستند. با Berlin Flex، هر نشستن تبدیل به تجربه‌ای از فرم و عملکرد می‌شود.',
-        urlParams: "id=3&title=Berlin Flex"
-    },
-
-    {
-        codeProduct: 4,
-        section: 'slider',
-        productName: 'Pacific Curve',
-        productStatus: 'موجود',
-        productPrice: 1000000,
-        productImg: [
-
-            'assets/img/Pacific Curve - 01.jpg',
-            'assets/img/Pacific Curve - 02.png',
-            'assets/img/Pacific Curve - 03.png'
-        ],
-        productDescription: 'مبل منحنی Pacific Curve، تجسمی‌ست از آرامش، رنگ، و طراحی شاعرانه. رنگ آبی عمیق آن، همچون اقیانوس پاسیفیک، فضا را به سکون و لطافت دعوت می‌کند. فرم منحنی و پف‌دار، آغوشی نرم برای لحظه‌های نشستن و خلوت است. میزهای طلایی و فرش نارنجی، تضادی هنرمندانه با رنگ مبل ایجاد کرده‌اند. طراحی این محصول، ترکیبی‌ست از مدرنیته و طبیعت زنده. مناسب برای فضاهایی که در پی خلاقیت، رنگ، و حس زندگی هستند. Pacific Curve نه فقط یک مبلمان، بلکه یک تجربه‌ی بصری و احساسی‌ست. با آن، هر نشستن تبدیل به لحظه‌ای از سفر به آرامش و زیبایی می‌شود.',
-        urlParams: "id=4&title=Pacific Curve"
-    },
-
-    {
-        codeProduct: 5,
-        section: 'slider',
-        productName: 'Oslo Arc',
-        productStatus: 'موجود',
-        productPrice: 1000000,
-        productImg: [
-
-            'assets/img/Oslo Arc - 01.jpg',
-            'assets/img/Oslo Arc - 02.png',
-            'assets/img/Oslo Arc - 03.png'
-        ],
-        productDescription: 'مبل منحنی Oslo Arc، تجسمی‌ست از آرامش، هندسه، و طراحی مینیمال. رنگ خاکستری عمیق آن، بافتی از سکون و وقار را به فضا هدیه می‌دهد. فرم منحنی و کوسن‌های مشکی، ترکیبی از نرمی و تضاد هنرمندانه را خلق کرده‌اند. میز گرد مات و عناصر دکوراتیو، فضایی متعادل و مدرن را کامل می‌کنند. محصولی مناسب برای فضاهایی که در پی سکوت، نظم، و زیبایی بی‌ادعا هستند. Oslo Arc نه فقط یک مبلمان، بلکه یک زبان بصری‌ست برای آرامش و تعادل. مناسب برای خانه‌های مینیمال، استودیوهای هنری، یا دفاتر مدرن با چاشنی لطافت. با Oslo Arc، هر نشستن تبدیل به لحظه‌ای از تأمل و زیبایی می‌شود.',
-        urlParams: "id=5&title=Oslo Arc"
-    }
-]
 
 let locationSearchParams = new URLSearchParams(location.search)
 let productIdParams = locationSearchParams.get('id')
@@ -112,16 +31,3 @@ if(sliderProducts){
     aboutProductDescription.textContent = sliderProducts.productDescription
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-//export default productsArray
