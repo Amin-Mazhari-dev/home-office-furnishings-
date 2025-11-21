fa817848b6fb5dae9dd1b72f8a69bc50534ac392
Amin Mazhari <amin.mazhari.1981@gmail.com>
2025-11-21 19:53:08 +0330

    Refactor (products-data.js)

--- assets/js/products-data.js
+++ assets/js/products-data.js
@@ -5,7 +5,7 @@ export let productsArray = [
         section: 'slider',
         productName: 'Siena Calm',
         productStatus: 'موجود',
-        productPrice: 1550000,
+        productPrice: 15500000,
         productImg: [
 
             'assets/img/Siena Calm - 01.jpg',
@@ -13,7 +13,8 @@ export let productsArray = [
             'assets/img/Siena Calm - 03.png'
         ],
         productDescription: 'مبل پنج ‌نفره‌ی Siena Calm، تجسمی از آرامش و سادگی‌ست؛ جایی برای نشستن، نفس کشیدن، و بازگشت به خویشتن. رنگ گرم و خاکی آن، یادآور غروب‌های سیه‌ناست؛ جایی که نور و سکوت در آغوش هم می‌نشینند. طراحی مینیمال با خطوط نرم و پایه‌های فلزی مشکی، ترکیبی از مدرنیته و لطافت را به فضای شما هدیه می‌دهد. پارچه‌ی مخملی و بافت‌دار آن، نه تنها چشم‌نواز است، بلکه حس لمس طبیعت را در دل خانه جاری می‌سازد. همراهی با میز سبز و گلدان ارکیده، این محصول را به نقطه‌ی کانونی هر اتاق تبدیل می‌کند. Siena Calm نه فقط یک مبل، بلکه یک دعوت است—به سکون، به زیبایی، به لحظه‌ای برای خود بودن. مناسب برای فضاهای مدرن، مینیمال، یا حتی کلاسیک با چاشنی آرامش و تعادل. با Siena Calm، هر نشستن، یک تجربه‌ی شاعرانه خواهد بود.',
-        urlParams: "id=1&title=Siena Calm"
+        urlParams: "id=1&title=Siena Calm",
+        counter: 1
     },
 
     {
@@ -21,7 +22,7 @@ export let productsArray = [
         section: 'slider',
         productName: 'California Puff',
         productStatus: 'موجود',
-        productPrice: 1700000,
+        productPrice: 17000000,
         productImg: [
 
             'assets/img/California Puff - 01.jpg',
@@ -29,7 +30,8 @@ export let productsArray = [
             'assets/img/California Puff - 03.png'
         ],
         productDescription: 'مبل و صندلی‌های California Puff، تجسمی از راحتی بی‌مرز و طراحی مدرن هستند. با رنگ کاراملی گرم و بافت چرمی لطیف، این محصول حس آغوشی نرم را به فضای شما می‌بخشد.طراحی پف‌دار و خطوط منحنی، آرامش را در هر نشستن جاری می‌کند.پایه‌های فلزی مشکی، تضادی هنرمندانه با فرم نرم و حجیم صندلی‌ها ایجاد کرده‌اند.مناسب برای فضاهای مدرن، مینیمال، یا حتی صنعتی با چاشنی گرما و صمیمیت. California Puff نه فقط یک مبلمان، بلکه یک تجربه‌ی لمسی و بصری‌ست.همراهی با نورپردازی ملایم و عناصر دکوراتیو، این محصول را به نقطه‌ی کانونی هر اتاق تبدیل می‌کند. با California Puff، هر لحظه‌ی نشستن، سفری‌ست به آرامش و زیبایی.',
-        urlParams: "id=2&title=California Puff"
+        urlParams: "id=2&title=California Puff",
+        counter: 1
     },
 
     {
@@ -45,7 +47,8 @@ export let productsArray = [
             'assets/img/Berlin Flex - 03.png'
         ],
         productDescription: 'سری مبلمان مدولار Berlin Flex، تجسمی از طراحی مینیمال و عملکرد هوشمندانه است. با رنگ مشکی کلاسیک و فرم‌های ساده، این محصول هر فضایی را به نهایت سادگی و زیبایی می‌رساند. قابلیت تبدیل شدن به لانژ، به شما آزادی می‌دهد که فضای خود را با لمس یک حرکت تغییر دهید. پایه‌های فلزی مشکی، ساختاری استوار و مدرن به طراحی این مبلمان بخشیده‌اند. محصولی مناسب برای خانه‌ها، ادارات، یا استودیوهایی که در جستجوی تعادل بین فرم و عملکرد هستند. Berlin Flex نه فقط یک مبلمان، بلکه یک هارمونی از نهایت سادگی و نهایت انعطاف است. مناسب برای فضاهای صنعتی، مدرن، یا حتی کوچک که در پی استفاده‌ی بهینه از فضا هستند. با Berlin Flex، هر نشستن تبدیل به تجربه‌ای از فرم و عملکرد می‌شود.',
-        urlParams: "id=3&title=Berlin Flex"
+        urlParams: "id=3&title=Berlin Flex",
+        counter: 1
     },
 
     {
@@ -53,7 +56,7 @@ export let productsArray = [
         section: 'slider',
         productName: 'Pacific Curve',
         productStatus: 'موجود',
-        productPrice: 1000000,
+        productPrice: 10000000,
         productImg: [
 
             'assets/img/Pacific Curve - 01.jpg',
@@ -61,7 +64,8 @@ export let productsArray = [
             'assets/img/Pacific Curve - 03.png'
         ],
         productDescription: 'مبل منحنی Pacific Curve، تجسمی‌ست از آرامش، رنگ، و طراحی شاعرانه. رنگ آبی عمیق آن، همچون اقیانوس پاسیفیک، فضا را به سکون و لطافت دعوت می‌کند. فرم منحنی و پف‌دار، آغوشی نرم برای لحظه‌های نشستن و خلوت است. میزهای طلایی و فرش نارنجی، تضادی هنرمندانه با رنگ مبل ایجاد کرده‌اند. طراحی این محصول، ترکیبی‌ست از مدرنیته و طبیعت زنده. مناسب برای فضاهایی که در پی خلاقیت، رنگ، و حس زندگی هستند. Pacific Curve نه فقط یک مبلمان، بلکه یک تجربه‌ی بصری و احساسی‌ست. با آن، هر نشستن تبدیل به لحظه‌ای از سفر به آرامش و زیبایی می‌شود.',
-        urlParams: "id=4&title=Pacific Curve"
+        urlParams: "id=4&title=Pacific Curve",
+        counter: 1
     },
 
     {
@@ -69,7 +73,7 @@ export let productsArray = [
         section: 'slider',
         productName: 'Oslo Arc',
         productStatus: 'موجود',
-        productPrice: 1000000,
+        productPrice: 10000000,
         productImg: [
 
             'assets/img/Oslo Arc - 01.jpg',
@@ -77,6 +81,7 @@ export let productsArray = [
             'assets/img/Oslo Arc - 03.png'
         ],
         productDescription: 'مبل منحنی Oslo Arc، تجسمی‌ست از آرامش، هندسه، و طراحی مینیمال. رنگ خاکستری عمیق آن، بافتی از سکون و وقار را به فضا هدیه می‌دهد. فرم منحنی و کوسن‌های مشکی، ترکیبی از نرمی و تضاد هنرمندانه را خلق کرده‌اند. میز گرد مات و عناصر دکوراتیو، فضایی متعادل و مدرن را کامل می‌کنند. محصولی مناسب برای فضاهایی که در پی سکوت، نظم، و زیبایی بی‌ادعا هستند. Oslo Arc نه فقط یک مبلمان، بلکه یک زبان بصری‌ست برای آرامش و تعادل. مناسب برای خانه‌های مینیمال، استودیوهای هنری، یا دفاتر مدرن با چاشنی لطافت. با Oslo Arc، هر نشستن تبدیل به لحظه‌ای از تأمل و زیبایی می‌شود.',
-        urlParams: "id=5&title=Oslo Arc"
+        urlParams: "id=5&title=Oslo Arc",
+        counter: 1
     }
 ]
