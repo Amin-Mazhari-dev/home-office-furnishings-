3f6f6abe72771126f1ef32fcd06047d47375a22d
Amin Mazhari <amin.mazhari.1981@gmail.com>
2025-11-30 17:24:19 +0330

    Improved basket cart position

--- assets/js/FixBasketPosition.js
+++ assets/js/FixBasketPosition.js
@@ -1,6 +1,8 @@
 const newbasketCardSection = document.querySelector('.basket-card-section')
+const basketHorizontalMenuHtmlIndex = document.querySelector('.basket-horizontalMenu-htmlIndex')
 
-function getElementTop(element) {
+ function getElementTop(element) {
+
     const style = getComputedStyle(element);
     const topValue = style.top; 
     
@@ -16,4 +18,6 @@ window.addEventListener('scroll', function(event){
 
     let newTop = currentTop + myscroll
     newbasketCardSection.style.top = newTop + "px";
+    basketHorizontalMenuHtmlIndex.style.top = newTop + "px";
 })
+
