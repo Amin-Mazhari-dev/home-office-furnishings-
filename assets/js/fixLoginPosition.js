31635498b61c085dc38d72aa649a275ab3c4fb4c
Amin Mazhari <amin.mazhari.1981@gmail.com>
2025-12-03 16:09:07 +0330

    Improved (fixLoginPosition.js) codes

--- assets/js/fixLoginPosition.js
+++ assets/js/fixLoginPosition.js
@@ -2,12 +2,15 @@ const fixPositionProduct = document.querySelector('.fix-position-product')
 
 function getElementTopValue(elem){
 
-    const styleElement = getComputedStyle(elem);
-    const elementTopValue = styleElement.top;
+    if(fixPositionProduct){
 
-    if(!elementTopValue || elementTopValue === 'auto') return 0;
-
-    return parseFloat(elementTopValue.replace(/px|rem|em|%/g, '')) || 0;
+        const styleElement = getComputedStyle(elem);
+        const elementTopValue = styleElement.top;
+    
+        if(!elementTopValue || elementTopValue === 'auto') return 0;
+    
+        return parseFloat(elementTopValue.replace(/px|rem|em|%/g, '')) || 0;
+    }
 }
 
 let currentTopValue = getElementTopValue(fixPositionProduct);
