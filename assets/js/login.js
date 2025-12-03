c55f184520d6e033f54a18b94ff2649da19c2f63
Amin Mazhari <amin.mazhari.1981@gmail.com>
2025-12-03 16:19:25 +0330

    Completed (login.js) codes

--- assets/js/login.js
+++ assets/js/login.js
@@ -57,3 +57,73 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 
 })
+
+// These codes are for validation and display password
+
+const userName = document.getElementById('user')
+const userNameMessage = document.querySelector('.userName-message')
+
+const userPassword = document.getElementById('pass')
+const passwordMessage = document.querySelector('.password-message')
+const eyeSlash = document.querySelector('.fa-eye-slash')
+const eye = document.querySelector('.fa-eye')
+
+const loginBtn = document.querySelector('.login-btn')
+
+
+function userNamevalidation(){
+
+    let len = userName.value.trim().length;
+
+    if(len < 3 || len > 15){
+
+        userNameMessage.textContent = 'نام کاربری باید حداقل 3 کراکتر و حداکثر 15 کراکتر باشد (فضای خالی یک کراکتر محسوب میشود)'
+        userNameMessage.style.color = 'rgb(104, 4, 4)'
+        eyeSlash.style.top = '170px'
+
+    }else{
+
+        userNameMessage.textContent = 'نام کاربری شما مناسب است'
+        userNameMessage.style.color = 'rgb(6, 82, 6)'
+    }
+}
+
+function userPasswordvalidation(){
+
+    let leng = userPassword.value.trim().length;
+
+    if(leng < 8 || leng > 12){
+
+        passwordMessage.textContent = 'رمز عبور باید حداقل 8 کراکتر و حداکثر 12 کراکتر باشد ( از ایجاد فضای خالی اجتناب کنید)'
+        passwordMessage.style.color = 'rgb(104, 4, 4)'
+
+    }else{
+
+        passwordMessage.textContent = 'رمز عبور شما مناسب است'
+        passwordMessage.style.color = 'rgb(6, 82, 6)'
+    }
+}
+
+function showPassword(){
+
+    let elementHasClass = eyeSlash.classList.contains('fa-eye-slash')
+
+    if(elementHasClass){
+
+        eyeSlash.classList.remove('fa-eye-slash')
+        eyeSlash.classList.add('fa-eye')
+        userPassword.setAttribute('type', 'text')
+
+    }else{
+
+        eyeSlash.classList.remove('fa-eye')
+        eyeSlash.classList.add('fa-eye-slash')
+        userPassword.setAttribute('type', 'password')
+    }
+    
+}
+
+
+userName.addEventListener('input', userNamevalidation)
+userPassword.addEventListener('input', userPasswordvalidation)
+eyeSlash.addEventListener('click', showPassword)
