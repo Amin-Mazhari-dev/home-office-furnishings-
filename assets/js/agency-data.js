f65b7831ba19126a8522f348ae81235d4ac6ffaf
Amin Mazhari <amin.mazhari.1981@gmail.com>
2026-01-04 17:01:02 +0330

    Added (agency-data.js) to project

---
+++ assets/js/agency-data.js
@@ -0,0 +1,69 @@
+export let agencyArray = [
+
+    {
+        agencyCity: 'Thran',
+        agencyImg: 'assets/img/Thran-agency.png',
+        agencyCode: 101,
+        agencyName: 'صـــالحـی مقــدم',
+        agencyManage: 'امیــر صـــالحـی مقــدم',
+        agencyTel: '447895412 - 021',
+        agencyAddress: 'تهران - خیابان آزادی - خیابان استاد معین',
+        urlParams: "id=101&title=نمایندگی صالحی مقدم"
+    },
+
+    {
+        agencyCity: 'Rasht',
+        agencyImg: 'assets/img/Rasht-agency.png',
+        agencyCode: 100,
+        agencyName: 'شعبه اصلی Royal City',
+        agencyManage: 'بنیـامین خیـرانـدیش',
+        agencyTel: '7727455 - 013',
+        agencyAddress: 'رشت - گلسار - بلوار گیلان',
+        urlParams: "id=100&title=نمایندگی شعبه اصلی Royal city"
+    },
+
+    {
+        agencyCity: 'Asfahan',
+        agencyImg: 'assets/img/Asfahan-agency.png',
+        agencyCode: 102,
+        agencyName: 'فــــلاحــی',
+        agencyManage: 'دانیال فــــلاحــی',
+        agencyTel: '87644152 - 031',
+        agencyAddress: 'اصفهان - سی و سه پل - چهار راه پزشک - گلستان 12',
+        urlParams: "id=102&title=نمایندگی فــــلاحــی"
+    },
+
+    {
+        agencyCity: 'Shiraz',
+        agencyImg: 'assets/img/Shiraz-agency.png',
+        agencyCode: 103,
+        agencyName: 'جهـان خـواه',
+        agencyManage: 'ابـراهیـم جهـان خـواه',
+        agencyTel: '43578942 - 071',
+        agencyAddress: 'خیابان حافظ - بلوار گلستان - جنب دانشگاه ادبیات',
+        urlParams: "id=103&title=نمایندگی جهان خواه"
+    },
+
+    {
+        agencyCity: 'Tabriz',
+        agencyImg: 'assets/img/Tabrize-agency.png',
+        agencyCode: 104,
+        agencyName: 'مهـرافـروز',
+        agencyManage: 'کامران مهـرافـروز',
+        agencyTel: '368555412 - 041',
+        agencyAddress: 'تبـریـز - بـازار بـزرگ تبـریـز',
+        urlParams: "id=104&title=نمایندگی مهرافروز"
+    },
+
+    {
+        agencyCity: 'Kish',
+        agencyImg: 'assets/img/Kish-agency.png',
+        agencyCode: 105,
+        agencyName: 'مهـــراد',
+        agencyManage: 'کـاوه مهـــراد',
+        agencyTel: '222456411 - 076',
+        agencyAddress: 'مـرکـز تجـاری اداری سـارینـا 1',
+        urlParams: "id=105&title=نمایندگی مهراد"
+    }
+
+] 
